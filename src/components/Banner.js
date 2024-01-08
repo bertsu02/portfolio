fb040e39913d8290b3d10d93d460b186c8d85661
Bartek Zmuda <bertsu@protonmail.com>
2024-01-08 15:37:19 +0100

    glassmorphism,flex,couple css changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,6 @@ import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap"
 import headerImg from "../assets/img/header-img1.svg";
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
-
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
@@ -44,18 +42,15 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <TrackVisibility>
-                            {({ isVisible }) =>
-                                <div className={isVisible ? "animated__animated animate__fadeIn" : ""}>
-                                    <div id="intro">
-                                    <h1>{`Hi I'm Bartek - `}<span className="wrap"><br></br>{text}</span></h1>
-                                    <p>I've been passionate about IT for a long time, but just recently I found out my calling in Front-End. I love to make unorthodox websites that leave people thinking - "How did he do that?". </p>
-                                    </div>
-                                </div>}
-                        </TrackVisibility>
+                        <div className="intro-container">
+                            <div className="intro">
+                                <h1 className="intro__name">{`Hi I'm Bartek - `}<span className="wrap"><br></br>{text}</span></h1>
+                                <p>I've been passionate about IT for a long time, but just recently I found out my calling in Front-End. I love to make unorthodox websites that leave people thinking - "How did he do that?". </p>
+                            </div>
+                        </div>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="Banner img" />
+                        <img src={headerImg} alt="Banner img" className="koi" />
                     </Col>
                 </Row>
             </Container>
