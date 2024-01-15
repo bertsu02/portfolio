f94f7fd1cef98b55da19f1b3b8cbfd02c7020a78
Bartek Zmuda <bertsu@protonmail.com>
2024-01-15 15:40:52 +0100

    added working icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon2 from "../assets/img/nav-icon2.png";
 
 export const Footer = () => {
   return (
@@ -12,9 +11,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="http://google.com"><img src={navIcon1} alt="Icon" /></a>
-              <a href="google.com"><img src={navIcon2} alt="Icon" /></a>
-              <a href="google.com"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/bartosz-%C5%BCmuda-a9083b2a9/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/bertsu02"><img src={navIcon2} alt="Icon" /></a>
             </div>
             <p>Copyright 2024. All Rights Reserved</p>
           </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,7 @@ import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from '../assets/img/logo.png.png'
 import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import navIcon2 from '../assets/img/nav-icon2.png'
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -43,9 +42,8 @@ export const NavBar = () => {
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="google.com"><img src={navIcon1} alt="" /></a>
-                            <a href="google.com"><img src={navIcon2} alt="" /></a>
-                            <a href="google.com"><img src={navIcon3} alt="" /></a>
+                            <a href="https://www.linkedin.com/in/bartosz-%C5%BCmuda-a9083b2a9/"><img src={navIcon1} alt="linkedin" /></a>
+                            <a href="https://github.com/bertsu02"><img src={navIcon2} alt="" /></a>
                         </div>
                         <button className="vvd" onClick={() => console.log('connect')}><span>Contact Me</span></button>
                     </span>
